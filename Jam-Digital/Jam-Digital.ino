b5a82a204f2a33142ae412913ca2e05a625e26a4
Sinekar Lintang <sinekarlintang@gmail.com>
2024-03-07 07:54:58 +0700

    initial commit

---
+++ Jam-Digital/Jam-Digital.ino
@@ -0,0 +1,26 @@
+void init_timer(){
+  cli(); // stop interrupts
+  TCCR1A = 0; // set entire TCCR1A register to 0
+  TCCR1B = 0; // same for TCCR1B
+  TCNT1  = 0; // initialize counter value to 0
+  // set compare match register for 1 Hz increments
+  OCR1A = 62499; // = 16000000 / (256 * 1) - 1 (must be <65536)
+  // turn on CTC mode
+  TCCR1B |= (1 << WGM12);
+  // Set CS12, CS11 and CS10 bits for 256 prescaler
+  TCCR1B |= (1 << CS12) | (0 << CS11) | (0 << CS10);
+  // enable timer compare interrupt
+  TIMSK1 |= (1 << OCIE1A);
+  sei(); // allow interrupts
+}
+ISR(TIMER1_COMPA_vect){
+   
+}
+void setup() {
+  init_timer();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
