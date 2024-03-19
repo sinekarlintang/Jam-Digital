8af58f224b8b7c03b5977501400846b77f17899b
rayvrico <124336396+rayvrico@users.noreply.github.com>
2024-03-19 10:36:11 +0700

    Update buzzer.ino

--- Jam-Digital/buzzer.ino
+++ Jam-Digital/buzzer.ino
@@ -1,1 +1,13 @@
+void loop() {
+  if (alarm_on == true) {
+    tone(buzzer, 1000);
+    delay(10000);
+    noTone(buzzer);
+    delay(3000);
+  }
+}
 
+// Interrupt service routine to set the silenceBuzzer variable
+void alarm_int() {
+  alarm_on = !alarm_on;
+}
